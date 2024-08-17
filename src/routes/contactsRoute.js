34d0b22e939923fcc59e390c5b20cbee25f5e57e
Roxaneryz <roxane5666@yahoo.gr>
2024-08-17 23:26:04 +0300

    +

--- src/routes/contactsRoute.js
+++ src/routes/contactsRoute.js
@@ -3,18 +3,19 @@ import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { getContacts, getContactById, createContact, updateContact, deleteContact } from '../controllers/contactsContrl.js';
 import { validateBody } from "../middlewares/validateBody.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
+import { validateID } from "../middlewares/validateID.js";
 
  const router = Router();
 
 router.get('/contacts', ctrlWrapper (getContacts));
-router.get('/contacts/:contactId', ctrlWrapper(getContactById));
+router.get('/contacts/:contactId', validateID, ctrlWrapper(getContactById));
 router.post(
   '/contacts',
   validateBody(createContactSchema),
   ctrlWrapper(createContact),
 );
-router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(updateContact));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContact));
+router.patch('/contacts/:contactId',validateID, validateBody(updateContactSchema), ctrlWrapper(updateContact));
+router.delete('/contacts/:contactId', validateID, ctrlWrapper(deleteContact));
 
 
 export default router;
